407c0d53e6470ddc41c645a09a0abfd320c58c96
Aditya_Paul <102823756+Aditya-Paul@users.noreply.github.com>
2022-04-27 21:00:35 +0600

    Add files via upload

--- BrickCalculator.js
+++ BrickCalculator.js
@@ -1,19 +1,28 @@
-
+ 
 function brickCalculator(floor){
-    for(i=0; i<floor; i++){
-      if(i<=10){
-          var oneToten = 15 * 10;
-          var brick = oneToten * floor; 
+
+      if(floor<=10){
+          var one_ten = 15 * 10;
+          var brick = one_ten * floor; 
           return brick;
       }
-      else if((i >= 11) && (i <= 20)){
-          var ten_twenty = 10 * 10;
-          var bricks = ten_twenty * floor ;
-          // var tbricks = brick + bricks;
+      else if(floor <= 20){
+          var a = 10*15*10;
+          var b = floor - 10;
+          var eleven_twenty = 10 * 10;
+          var bricks = eleven_twenty * b + a ;
+          return bricks;
       }
-      var tbricks = brick + bricks;
-       return tbricks;
+      else {
+        var x = 10*15*10;
+        var y = 10*10*10;
+        var b = floor - 20;
+        var twentyone_hundred = 8 * 10;
+        var bricks = twentyone_hundred * b + x + y ;
+        return bricks;
+    }
+      
   }
-  }
-  console.log(brickCalculator(12))
+
+  console.log(brickCalculator(22))
   
